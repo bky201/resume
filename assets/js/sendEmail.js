b234942c67efccd181119ba9f84fac9c040ac01a
bek <bekrye@gmail.com>
2023-06-26 02:02:48 +0200

    Implement emailjs on cpntact form

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,18 @@
+function sendMail(contactForm) {
+    emailjs.send('service_jk23o43', 'template_fdi9u7s', {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "message": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
+
+
